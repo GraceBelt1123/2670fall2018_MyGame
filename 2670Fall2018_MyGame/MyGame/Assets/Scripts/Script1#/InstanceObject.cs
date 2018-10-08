3b50b17bff566ca973d42c54307afa1ff1c1fdbe
Unknown <undergo4570@gmail.com>
2018-10-08 14:18:17 -0600

    2670 Committed Save.

---
+++ 2670Fall2018_MyGame/MyGame/Assets/Scripts/Script1#/InstanceObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArtisonDream.Experimental
+{
+[CreateAssetMenu]
+public class InstanceObject : ScriptableObject
+{
+
+    public void Call(GameObject instance)
+    {
+        Instantiate(instance);
+    }
+}
+}
