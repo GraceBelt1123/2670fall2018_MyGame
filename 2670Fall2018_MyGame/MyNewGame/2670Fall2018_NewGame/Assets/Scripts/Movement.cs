3d51b4d77beb77c54076b7fbaaf5dd08eb1d1cc7
Unknown <undergo4570@gmail.com>
2018-10-19 16:48:59 -0600

    Committed

---
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Boo.Lang;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float Speed = 5f;
+    public bool iControl;
+    public float Jump = 10.0f;
+
+    void Update()
+    {
+        if (iControl == true)
+        {
+            float sweet = Input.GetAxis("Horizontal") * Speed;
+            Player.transform.Translate(sweet * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * Jump);
+    }
+}
+
+    
+
