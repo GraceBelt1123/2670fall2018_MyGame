df1a36956d112197883c35791d386eccc3e867f0
Unknown <undergo4570@gmail.com>
2018-10-24 17:00:37 -0600

    Committed

---
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/TimeSet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Boo.Lang;
+using UnityEngine;
+
+
+public class TimeSet : MonoBehaviour
+{
+	public int countDown = 0;
+	public string[] Sweets;
+	
+//This is countdown in 30 seconds when it is pressed the button "Play".
+	void Start () {
+
+		while (countDown < 30)
+		{
+			print(countDown);
+			countDown += 0;
+		}
+
+		while (countDown < Sweets.Length)
+		{
+			print(Sweets[countDown]);
+			countDown++;
+		}
+		
+		Console.WriteLine("Go");
+		Console.WriteLine(Sweets[0]);
+		Console.WriteLine(Sweets[1]);
+		Console.WriteLine(Sweets[2]);
+		Console.WriteLine(Sweets[3]);
+		Console.WriteLine(Sweets[4]);
+		Console.WriteLine(Sweets[5]);
+		Console.WriteLine(Sweets[6]);
+		Console.WriteLine(Sweets[7]);
+		Console.WriteLine(Sweets[8]);
+		Console.WriteLine(Sweets[9]);
+		Console.WriteLine(Sweets[10]);
+		Console.WriteLine(Sweets[11]);
+		Console.WriteLine(Sweets[12]);
+		Console.WriteLine(Sweets[13]);
+		Console.WriteLine(Sweets[14]);
+		Console.WriteLine(Sweets[15]);
+		Console.WriteLine(Sweets[16]);
+		Console.WriteLine(Sweets[17]);
+		Console.WriteLine(Sweets[18]);
+		Console.WriteLine(Sweets[19]);
+		Console.WriteLine(Sweets[20]);
+		Console.WriteLine(Sweets[21]);
+		Console.WriteLine(Sweets[22]);
+		Console.WriteLine(Sweets[23]);
+		Console.WriteLine(Sweets[24]);
+		Console.WriteLine(Sweets[25]);
+		Console.WriteLine(Sweets[26]);
+		Console.WriteLine(Sweets[27]);
+		Console.WriteLine(Sweets[28]);
+		Console.WriteLine(Sweets[29]);
+		Console.WriteLine(Sweets[30]);
+	}
+}
