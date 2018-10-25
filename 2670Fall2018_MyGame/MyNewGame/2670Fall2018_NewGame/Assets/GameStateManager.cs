c1c0541cfc9db68d51bf623ca429fc78df9fdcfc
Unknown <undergo4570@gmail.com>
2018-10-25 16:47:25 -0600

    Committed

---
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/GameStateManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+	public GameObject winText;
+	public GameObject loseText;
+	public bool didWin;
+	public int score;
+
+
+	//do stuff when the timer is started
+	public void StartGame()
+	{
+		print("started game");
+		winText.SetActive(false);
+		loseText.SetActive(false);
+	}
+
+	//do stuff when the timer has ended
+	public void EndGame()
+	{
+
+		//if score is greater than 5, set didWin equal to true
+		//if score is less than 5, set didWin equal to false
+
+		print("ended game");
+		if(didWin)
+		{
+			winText.SetActive(true);
+		}
+		
+		if (!didWin)
+		{
+			loseText.SetActive(true);
+		}
+
+	}
+
+
+}
--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/TimeSet.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/TimeSet.cs
@@ -1,59 +1,39 @@
 ﻿using System;
-using Boo.Lang;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Analytics;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 
 public class TimeSet : MonoBehaviour
 {
-	public int countDown = 0;
-	public string[] Sweets;
-	public bool Timing;
+	public int startingNumber = 5;
+
+	public Text text;
+
+	public UnityEvent startTimerEvent;
+	public UnityEvent endTimerEvent;
 	
-//This is countdown in 30 seconds when it is pressed the button "Play".
-	void Update () {
+	//This is countdown in 30 seconds when it is pressed the button "Play".
 
-		while (countDown < 30.0f)
+	public void StartCountDown()
+	{
+		StartCoroutine(CountDown());
+	}
+
+	IEnumerator CountDown()
+	{
+		startTimerEvent.Invoke();
+		int tempStartingNum = startingNumber;
+
+		while(tempStartingNum > 0)
 		{
-			
+			tempStartingNum--;
+			text.text = tempStartingNum.ToString();
+			yield return new WaitForSeconds(1);	
 		}
 
-		while (countDown < Sweets.Length)
-		{
-			
-		}
-		
-		Console.WriteLine("Go");
-		Console.WriteLine(Sweets[0]);
-		Console.WriteLine(Sweets[1]);
-		Console.WriteLine(Sweets[2]);
-		Console.WriteLine(Sweets[3]);
-		Console.WriteLine(Sweets[4]);
-		Console.WriteLine(Sweets[5]);
-		Console.WriteLine(Sweets[6]);
-		Console.WriteLine(Sweets[7]);
-		Console.WriteLine(Sweets[8]);
-		Console.WriteLine(Sweets[9]);
-		Console.WriteLine(Sweets[10]);
-		Console.WriteLine(Sweets[11]);
-		Console.WriteLine(Sweets[12]);
-		Console.WriteLine(Sweets[13]);
-		Console.WriteLine(Sweets[14]);
-		Console.WriteLine(Sweets[15]);
-		Console.WriteLine(Sweets[16]);
-		Console.WriteLine(Sweets[17]);
-		Console.WriteLine(Sweets[18]);
-		Console.WriteLine(Sweets[19]);
-		Console.WriteLine(Sweets[20]);
-		Console.WriteLine(Sweets[21]);
-		Console.WriteLine(Sweets[22]);
-		Console.WriteLine(Sweets[23]);
-		Console.WriteLine(Sweets[24]);
-		Console.WriteLine(Sweets[25]);
-		Console.WriteLine(Sweets[26]);
-		Console.WriteLine(Sweets[27]);
-		Console.WriteLine(Sweets[28]);
-		Console.WriteLine(Sweets[29]);
-		Console.WriteLine(Sweets[30]);
+		endTimerEvent.Invoke();
+		text.text = "Restart";
 	}
 }
