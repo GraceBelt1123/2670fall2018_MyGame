8f97501cabbd0f2f3d077cc7968a6855c877d343
Unknown <undergo4570@gmail.com>
2018-10-27 13:31:35 -0600

    Committed

--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/FallingCoins.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/FallingCoins.cs
@@ -9,16 +9,30 @@ public class FallingCoins : MonoBehaviour
     //It will be repeated the same thing over and over again. 
     
     public GameObject Coins;
-    public float FallSpeed = 2.0f;
+    public float FallSpeed = 10.0f;
+    public float SecondSpeed = 10.0f;
+    public float ThirdSpeed = 10.0f;
 
     void Update()
     {
         Falling();
+        secondFalling();
+        thirdFalling();
     }
 
     public void Falling()
     {
         transform.Translate(Vector3.down * FallSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void secondFalling()
+    {
+        transform.Translate(Vector3.down * SecondSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void thirdFalling()
+    {
+        transform.Translate(Vector3.down * ThirdSpeed * Time.deltaTime, Space.World );
     }
 }
 
