7447ffe210a983b8f73317ba2eca79e6977a8de6
Unknown <undergo4570@gmail.com>
2018-10-29 13:42:04 -0600

    Committed

--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/CreateMultCoins.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/CreateMultCoins.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CreateMultCoins : MonoBehaviour
 {
+    //This is to make Multiply Coins to be kept falling down above. 
+    public GameObject Prefab;
+    
     void Start()
     {
-        //GameObject prefab = Prefab.Load("CoinOne") as GameObject;
-       // for (int i = 0; i < 100; i++)
-      //  {
-        //    GameObject go = Instantiate(prefab) as GameObject;
-          //  go.transform.position = new Vector3(0, i * 5, 0);
-        //}
+       // GameObject prefab = Prefab.Load("CoinOne") as GameObject;
+        for (int i = 0; i < 100; i++)
+        {
+           GameObject go = Instantiate(Prefab);
+            go.transform.position = new Vector3(0, i * 5, 0);
+        }
     }
 }
 
