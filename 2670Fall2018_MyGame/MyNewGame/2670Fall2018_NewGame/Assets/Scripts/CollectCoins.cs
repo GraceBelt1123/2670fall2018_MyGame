0f6a3a362ce94429ee47b223e6c35f3f5954191b
Unknown <undergo4570@gmail.com>
2018-10-29 21:54:28 -0600

    Committed

--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/CollectCoins.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/CollectCoins.cs
@@ -4,16 +4,50 @@ using UnityEditor.Experimental.Build;
 using UnityEngine;
 
 public class CollectCoins : MonoBehaviour {
- //This to make the Player collect coins, related to the score. 
+ //Counting numbers when is entering the Player. 
 
-    public float Money;
-    
-    public void OnTriggerEnter(Collider other)
+ public GameObject Player;
+ public Vector3 iValue;
+ public int sweetCount;
+ public float sweetWait;
+ public float startWait;
+ public float sparkWait;
+
+ public GUIText sweetText;
+ private int score;
+
+ void Start()
+ {
+  score = 0;
+  UpdateScore();
+  StartCoroutine(LuckBrain());
+ }
+
+ IEnumerable LuckBrain()
+ {
+     yield return new WaitForSeconds(startWait);
+     while (true)
+     {
+         for (int i = 0; i < sweetCount; i++)
+         {
+             Vector3 tomPosition = new Vector3(Random.Range(-iValue.x, iValue.x), iValue.y, iValue.z);
+             Quaternion iRotation = Quaternion.identity;
+             Instantiate((Player, tomPosition, iRotation));
+             yield return new WaitForSeconds(sparkWait);
+         }
+
+         yield return new WaitForSeconds(sparkWait);
+     }
+ }
+
+    public void Addscore(int newValue)
     {
-        print("Store");//It worked.
+        score += newValue;
+        UpdateScore();
     }
 
-    public void CoinCollected()
+    void UpdateScore()
     {
+        sweetText.text = "Score: " + score;
     }
 }
--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Score.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Score.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-  public void EndScore()
+  //Game Contoller in this script could be used for score. 
+  public GameObject Greater;
+  public GameObject Less;
+  public bool LuckScore;
   
+  public void EndScore()
+  {
+    if (LuckScore)
+    {
+      Greater.SetActive(true);
+    }
+
+    if (!LuckScore)
+    {
+      Less.SetActive(true);
+    }
+  }
 }
+
