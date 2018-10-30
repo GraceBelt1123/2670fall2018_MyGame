22cbba15ece8b21c0a3ce467f9a71082008c1bf4
Unknown <undergo4570@gmail.com>
2018-10-29 21:54:08 -0600

    Committed

--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Score.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Score.cs
@@ -1,31 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-  //Game Contoller in this script could be used for score. 
-  public GameObject Greater;
-  public GameObject Less;
-  public bool LuckScore;
-  public Text text;
-  public int whatscore;
-  public UnityEvent endLuck;
-  
   public void EndScore()
-  {
-    if (LuckScore)
-    {
-      Greater.SetActive(true);
-    }
-
-    if (!LuckScore)
-    {
-      Less.SetActive(true);
-    }
-  }
+  
 }
-
