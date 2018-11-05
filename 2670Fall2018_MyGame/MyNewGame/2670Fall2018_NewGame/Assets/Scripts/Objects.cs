e81d6db41bcf67d04a627af967a150f753e00566
Unknown <undergo4570@gmail.com>
2018-11-05 16:20:38 -0700

    Committed

---
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Objects.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects",)]
+public class Objects : MonoBehaviour {
+
+}
---
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/PurchasableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Purchase", menuName = "Store/Purchasable")]
+
+public class PurchasableObject : MonoBehaviour
+{
+
+    public Object Item;
+    public int Value;
+    public bool Perpetual;
+    public bool Instanciatable;
+
+    public void CreateItem()
+    {
+        if (Instanciatable && Item is GameObject)
+        {
+            Instanciatable(Item);
+        }
+    }
+
+}
