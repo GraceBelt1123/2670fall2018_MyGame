29f1c736b730f1db6f0519511114d0b4ba746acf
Unknown <undergo4570@gmail.com>
2018-12-04 14:49:08 -0700

    Committed

--- 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Store.cs
+++ 2670Fall2018_MyGame/MyNewGame/2670Fall2018_NewGame/Assets/Scripts/Store.cs
@@ -10,39 +10,10 @@ public class Store : ScriptableObject
 	public PurchasableObject Purchased;
 	public IntData Cash;
 	public int TotalValue = 3000;
-
+	public GameObject Button;
+	public GameObject Canvas;
 	public UnityEvent MadePurchase;
-
-	public void MakePurchase(PurchasableObject obj)
-	{
-		PurchasableObject availableObject = Available.ObjectList[i] as PurchasableObject;
-		for (var i = 0; i < Available.ObjectList.Count; i++)
-		{
-			if (availableObject == obj && Cash.Value >= availableObject.Value)
-			{
-				Cash.Value -= availableObject.Value;
-				Purchased.ObjectList.Add(obj);
-				Available.ObjectList.Remove(availableObject);
-				MadePurchase.Invoke();
-			}
-		}
-	}
-
-	public void PurchaseALL()
-	{
-		if (Cash.Value >= TotalValue)
-		{
-			Cash.Value -= TotalValue;
-			for (var i = 0; i < Available.ObjectList.Count; i++)
-			{
-				var item = Available.ObjectList[i];
-				Purchased.ObjectList.Add(item);
-			}
-
-			Available.ObjectList.Clear();
-		}
-	}
-
+	
 	private void OnEnable()
 	{
 		TotalValue = 0;
@@ -53,5 +24,52 @@ public class Store : ScriptableObject
 		}
 
 		TotalValue %= 75;
+	}
+
+	public void BuildUI()
+	{
+		var newCanvase = Instantiate(Canvas);
+		var newButton = Instantiate(Button, newCanvase.transform);
+		var buttonComponent = newButton.GetComponent<Button>(0);
+		buttonComponent.onClick.AddListener(PurchaseButton);
+		var label = newButton.GetComponentInChildren<Text>();
+		label.text = Available.ObjectList[0].name;
+	}
+
+	private void PurchaseButton()
+	{
+		
+	}
+	
+	public void MakePurchase(PurchasableObject obj)
+	{
+		for (var i = 0; i < Available.ObjectList.Count; i++)
+		{
+			var availableObject = Available.ObjectList[i];
+			if (availableObject != obj || Cash.Value < availableObject.Value) continue;
+			Cash.Value -= availableObject.Value;
+			Purchased.ObjectList.Add(obj);
+
+			if (availableObject.Perpetual)
+			{
+				Available.ObjectList.Remove(availableObject);
+			}
+
+			MadePurchase.Invoke();
+		}
+	}
+
+	public void PurchaseAll()
+	{
+		if (Cash.Value >= TotalValue)
+		{
+			Cash.Value -= TotalValue;
+			for (var i = 0; i < Available.ObjectList.Count; i++)
+			{
+				var item = Available.ObjectList[i];
+			}
+
+			Available.ObjectList.Clear();
+		}
 	}
 }
